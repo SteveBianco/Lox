5692f981d7a0a1053365d8a2df673c8c525ff449
SteveBianco <steven.bianco@gmail.com>
2022-05-01 11:34:58 -0400

    For now support literal tokens with a variant in the token class and specialized constructlrs for each literal type. Later may move to derived types

--- Lox/JLox/Token.cpp
+++ Lox/JLox/Token.cpp
@@ -5,6 +5,43 @@ Token::Token(TokenType type, const std::string& lexeme, int line):
 {
 }
 
+Token::Token(const std::string& lexeme, std::string literalValue, int line):
+	Token(TokenType::STRING, lexeme, line)
+{
+	literal_ = literalValue;
+}
+
+Token::Token(const std::string& lexeme, double literalValue, int line) :
+	Token(TokenType::NUMBER, lexeme, line)
+{
+	literal_ = literalValue;
+}
+
+TokenType Token::getType() const
+{
+	return type_;
+}
+
+const std::string& Token::getLexeme() const
+{
+	return lexeme_;
+}
+
+int Token::getLineNumber() const
+{
+	return line_;
+}
+
+std::string Token::getStringLiteral() const
+{
+	return std::get<std::string>(literal_);
+}
+
+double Token::getNumericLiteral() const
+{
+	return std::get<double>(literal_);
+}
+
 Token::operator std::string() const
 {
 	// TODO include type and literal value (if any)

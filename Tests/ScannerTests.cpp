1375ad3e23e1e05ef90d317f4efef02250b7fd98
SteveBianco <steven.bianco@gmail.com>
2022-05-14 15:07:02 -0400

    add some scanner tests

--- Tests/ScannerTests.cpp
+++ Tests/ScannerTests.cpp
@@ -5,6 +5,15 @@
 #include "Scanner.h"
 #include "ErrorRecorder.h"
 
+#include <vector>
+#include <unordered_map>
+#include <memory>
+
+using std::vector;
+using std::unordered_map;
+using std::unique_ptr;
+using enum TokenType;
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace Tests
@@ -17,20 +26,95 @@ namespace Tests
 		{
 			errors++;
 		}
+
+		void error(const Token& token, const std::string& errorMessage) override
+		{
+
+		}
 	};
 
 	TEST_CLASS(ScannerTests)
 	{
 	public:
 
-		TEST_METHOD(ScannerTest1)
+		TEST_METHOD(AllSingleCharacterTokensAppearOnce)
 		{
-			std::string source = "area = (b + c * d)";
+			const std::string source = "{} () ,. -+ ;*/ = < >";
+
 			MockErrorRecorder errors;
 			Scanner scanner{ source, errors };
 			const auto& tokens = scanner.scan();
 
-			Assert::IsTrue(tokens.size() == 10);
+			validateTokenCountsAllEqualOne(tokens);
+		}
+
+		TEST_METHOD(AllTokensAppearOnce)
+		{
+			const std::string source = "<= >= == !=";
+
+			MockErrorRecorder errors;
+			Scanner scanner{ source, errors };
+			const auto& tokens = scanner.scan();
+
+			const int expectedTokenCount = 5; // inlcudes eof token
+			Assert::AreEqual(expectedTokenCount, (int)tokens.size());
+			validateTokenCountsAllEqualOne(tokens);
+		}
+
+		TEST_METHOD(ShouldIgnoreComment)
+		{
+			const std::string source = " \" A STRING LITERAL \" // followd by a comment 3*4+5/6";
+
+			MockErrorRecorder errors;
+			Scanner scanner{ source, errors };
+			const auto& tokens = scanner.scan();
+
+			const int expectedTokenCount = 2; // a string and an eof token
+			Assert::AreEqual(expectedTokenCount, (int)tokens.size());
+			
+			auto counts = countByType(tokens);
+			Assert::AreEqual(1, counts[STRING]);
+		}
+
+		unordered_map<TokenType, int> countByType(const vector<unique_ptr<Token>>& tokens)
+		{
+			unordered_map<TokenType, int> actualCounts;
+
+			for (const auto& token : tokens)
+			{
+				++actualCounts[token->getType()];
+			}
+
+			return actualCounts;
+		}
+
+		void validateTokenCounts(const vector<unique_ptr<Token>>& tokens, const unordered_map<TokenType, int>& expectedCounts)
+		{
+			unordered_map<TokenType, int> actualCounts;
+
+			for (const auto& token : tokens)
+			{
+				++actualCounts[token->getType()];
+			}
+
+			Assert::IsTrue(actualCounts == expectedCounts);
+		}
+
+		void validateTokenCountsAllEqualOne(const vector<unique_ptr<Token>>& tokens)
+		{
+			unordered_map<TokenType, int> actualCounts;
+
+			for (const auto& token : tokens)
+			{
+				++actualCounts[token->getType()];
+			}
+
+			for (const auto& entry : actualCounts)
+			{
+				Assert::AreEqual(1, entry.second);
+			}
 		}
 	};
+
+	
 }
